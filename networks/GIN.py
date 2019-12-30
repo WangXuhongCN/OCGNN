4062e0935bd3a83b62bc5a9634c7975decd7bca0
WangXuhongCN <34096632+WangXuhongCN@users.noreply.github.com>
2019-12-30 22:03:58 +0800

    取消GIN的bias

--- networks/GIN.py
+++ networks/GIN.py
@@ -53,17 +53,17 @@ class MLP(nn.Module):
             raise ValueError("number of layers should be positive!")
         elif num_layers == 1:
             # Linear model
-            self.linear = nn.Linear(input_dim, output_dim)
+            self.linear = nn.Linear(input_dim, output_dim,  bias=False)
         else:
             # Multi-layer model
             self.linear_or_not = False
             self.linears = torch.nn.ModuleList()
             self.batch_norms = torch.nn.ModuleList()
 
-            self.linears.append(nn.Linear(input_dim, hidden_dim))
+            self.linears.append(nn.Linear(input_dim, hidden_dim,  bias=False))
             for layer in range(num_layers - 2):
-                self.linears.append(nn.Linear(hidden_dim, hidden_dim))
-            self.linears.append(nn.Linear(hidden_dim, output_dim))
+                self.linears.append(nn.Linear(hidden_dim, hidden_dim,  bias=False))
+            self.linears.append(nn.Linear(hidden_dim, output_dim,  bias=False))
 
             for layer in range(num_layers - 1):
                 self.batch_norms.append(nn.BatchNorm1d((hidden_dim)))
